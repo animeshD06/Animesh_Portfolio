34f09634b893665d8a2839c6d3d928f62c733b96
animeshD06 <animeshabhidubey182@GMAIL.COM>
2026-01-18 15:31:03 +0530

    updated projects

--- src/constant/projects.ts
+++ src/constant/projects.ts
@@ -1,11 +1,22 @@
 export const projectsData = [
   {
-    name: "AN",
+    name: "Portfolio",
     description:
       "A personal portfolio built with a focus on performance, SEO, and analytics. Includes structured metadata, full integration with modern analytics tools, and scores 91+ in real-world performance tests.",
-    github_link: "",
-    demo: "/",
+    github_link: "https://github.com/animeshD06/Animesh_Portfolio",
+    demo: "animeshportfolio.vercel.app",
     tech: ["Framer Motion", "EmailJS", "Vercel Analytics"],
+  },
+  
+];
+export const projectsData2 = [
+  {
+    name: "Crowd management software",
+    description:
+      "A real-time AI-powered crowd management system with detection, tracking, heatmap visualization, and safety analytics for events and public spaces",
+    github_link: "https://github.com/animeshD06/Crowd-Management-System-software.git",
+    demo: "/",
+    tech: ["YOLO11x", "OpenCV", "Python", "PYTORCH"],
   },
   
 ];
